071e2b01b360019ececadbe40d2eb9fe8813e95d
Pizer Chen <pizer.chen@mgail.com>
2021-08-06 19:35:10 +0800

    console frontend adjust

--- point-console/src/main/resources/static/app/main.js
+++ point-console/src/main/resources/static/app/main.js
@@ -1,28 +1,43 @@
-System.register(["element-plus", './style.css'], function(_e, _c){
-    var ElementPlus
-    var Style;
+System.register(["element-plus", './style.css', 'agent-list'], function(_e, _c){
+    let ElementPlus;
+    let AgentList;
     return {
-        setters: [function(_){ElementPlus = _;}, function(_){document.adoptedStyleSheets = [...document.adoptedStyleSheets, _.default];}],
+        setters: [
+            function(_){ElementPlus = _;},
+            function(_){document.adoptedStyleSheets = [...document.adoptedStyleSheets, _.default];},
+            function(_){AgentList = _;}
+        ],
         execute: function () {
             const App = {
                 data() {
-                    const item = {
-                        date: '2016-05-02',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    };
                     return {
-                        tableData: Array(20).fill(item)
-                    }
+                        activeIndex: '1'
+                    };
                 },
                 mounted() {
                     setInterval(() => {
                         this.counter++
                     }, 1000)
+                },
+                methods:{
+                    handleSelect(key, keyPath) {
+                        console.log(key, keyPath);
+                    }
                 }
             }
 
+
+            const routes=[
+                {path:'/agent-list', component: AgentList},
+            ]
+
+            const router = VueRouter.createRouter({
+                history: VueRouter.createWebHashHistory(),
+                routes,
+            })
+
             const app = Vue.createApp(App)
+            app.use(router)
             app.use(ElementPlus)
             app.mount('#app')
         }
---
+++ point-console/src/main/resources/static/components/agent-list/main.js
@@ -0,0 +1,10 @@
+System.register([], function(_e, _c){
+    return {
+        setters:[],
+        execute:function(){
+            _e({
+                template:'<div>Agent List</div>'
+            });
+        }
+    }
+});
